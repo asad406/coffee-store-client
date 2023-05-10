6b75e147d4d1526460baec9a730a83ad0ed69917
asad406 <asadl76@gmail.com>
2023-05-10 09:00:49 +0600

    basic router setup

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>Add a Coffee</h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
